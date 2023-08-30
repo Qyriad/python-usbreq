e23b83302da57442663100c715f152c3c8765290
Qyriad <qyriad@qyriad.me>
2023-08-29 23:58:55 -0600

    make flake.nix more complete

--- flake.nix
+++ flake.nix
@@ -3,18 +3,45 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  # FIXME: add a package output too
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShell {
-        propagatedBuildInputs = with pkgs.python3Packages; [
-          pyusb
-          inflection
-          ipython
-        ];
+      inherit (pkgs) python3Packages;
+
+      dependencies = with python3Packages; [
+        pyusb
+        inflection
+      ];
+
+      devDependencies = with python3Packages; [
+        sphinx
+        sphinx-rtd-theme
+      ];
+
+      usbreqPkg = python3Packages.buildPythonPackage {
+        pname = "usbreq";
+        version = "0.2.1";
+        src = ./.;
+
+        format = "pyproject";
+
+        pythonImportsCheck = [ "usbreq" ];
+
+        propagatedBuildInputs = dependencies;
+        buildInputs = with python3Packages; [ setuptools wheel ];
       };
+    in {
+
+      # IPython is a much nicer development experience.
+      devShells.default = pkgs.mkShell {
+        propagatedBuildInputs = dependencies ++ devDependencies ++ [ python3Packages.ipython ];
+      };
+
+      devShells.minimal = pkgs.mkShell {
+        propagatedBuildInputs = dependencies;
+      };
+
+      packages.default = usbreqPkg;
     }
   );
 }
