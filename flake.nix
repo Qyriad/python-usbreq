b900e722e75c1bcc28b85bae35a2da9789015ced
Qyriad <qyriad@qyriad.me>
2023-08-31 15:10:28 -0600

    greatly improve flake.nix

--- flake.nix
+++ flake.nix
@@ -6,42 +6,63 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      inherit (pkgs) python3Packages;
 
-      dependencies = with python3Packages; [
-        pyusb
-        inflection
-      ];
+      intersectLists = pkgs.lib.lists.intersectLists;
+      forEach = pkgs.lib.lists.forEach;
 
-      devDependencies = with python3Packages; [
-        sphinx
-        sphinx-rtd-theme
-      ];
+      # Like "lib.lists.intersectLists" but takes a list of lists instead of two list arguments.
+      intersectAll = builtins.foldl' (a: b: a b) intersectLists;
 
-      usbreqPkg = python3Packages.buildPythonPackage {
+      # Takes a list of packages and returns the intersection of all platforms supported by those packages.
+      intersectPlatforms = pkgs: intersectAll (forEach pkgs (pkg: pkg.meta.platforms));
+
+      usbreq = pkgs.python3Packages.buildPythonPackage rec {
         pname = "usbreq";
         version = "0.2.1";
+        format = "pyproject";
         src = ./.;
 
-        format = "pyproject";
+        meta = {
+          description = "A USB library for humans";
+          platforms = intersectPlatforms (with pkgs.python3Packages; [ pyusb inflection ]);
+        };
 
         pythonImportsCheck = [ "usbreq" ];
 
-        propagatedBuildInputs = dependencies;
-        buildInputs = with python3Packages; [ setuptools wheel ];
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          pyusb
+          inflection
+        ];
+
+        nativeBuildInputs = with pkgs.python3Packages; [
+          setuptools
+          wheel
+        ];
+
+        # I guess documentation builders are check inputs?
+        nativeCheckInputs = with pkgs.python3Packages; [
+          sphinx
+          sphinx-rtd-theme
+        ];
       };
+
     in {
+      packages.default = usbreq;
 
       # IPython is a much nicer development experience.
       devShells.default = pkgs.mkShell {
-        propagatedBuildInputs = dependencies ++ devDependencies ++ [ python3Packages.ipython ];
+
+        inputsFrom = [ usbreq ];
+
+        packages = with pkgs.python3Packages; [
+          build
+          twine
+          ipython
+        ];
+
       };
 
-      devShells.minimal = pkgs.mkShell {
-        propagatedBuildInputs = dependencies;
-      };
-
-      packages.default = usbreqPkg;
     }
+
   );
 }
